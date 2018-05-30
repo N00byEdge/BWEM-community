cbdc23bf475c94aea8dfb22a189910d4d6df49bc
Evan Driscoll <evaned@gmail.com>
2018-05-29 22:15:13 -0500

    Change tuple to struct (MapResourcesTestParameters)

--- Tests/MapResourcesInfo.cpp
+++ Tests/MapResourcesInfo.cpp
@@ -6,10 +6,16 @@
 
 #include "gtest/gtest.h"
 
-typedef std::tuple<
-	std::string,
-	int,
-	int> MapResourcesTestParameters;
+struct MapResourcesTestParameters
+{
+	std::string mapName;
+	int expectedMinerals;
+	int expectedGeysers;
+
+	MapResourcesTestParameters(const char * n, int g, int m)
+		: mapName(n), expectedMinerals(m), expectedGeysers(g)
+	{}
+};
 
 class MapResourcesInfo : public ::testing::TestWithParam<MapResourcesTestParameters> {
 };
@@ -18,8 +24,8 @@ TEST_P(MapResourcesInfo, MapGeyserCount) {
 	BWEM::detail::MapImpl map;
 	
 	auto param = GetParam();
-	auto mapName = std::get<0>(param);
-	auto expectedGeysersCount = std::get<1>(param);
+	auto mapName = param.mapName;
+	auto expectedGeysersCount = param.expectedGeysers;
 	auto countedGeysers = 0;
 
   runOnMap(mapName, [&](auto game) {
@@ -38,8 +44,8 @@ TEST_P(MapResourcesInfo, MapMineralCount) {
 	BWEM::detail::MapImpl map;
 
 	auto param = GetParam();
-	auto mapName = std::get<0>(param);
-	auto expectedMineralsCount = std::get<2>(param);
+	auto mapName = param.mapName;
+	auto expectedMineralsCount = param.expectedMinerals;
 	auto countedMinerals = 0;
   runOnMap(mapName, [&](auto game) {
     map.Initialize(game);
@@ -63,19 +69,19 @@ INSTANTIATE_TEST_CASE_P(
 	SynteticCheck,
 	MapResourcesInfo,
 	::testing::ValuesIn(getTestData({
-		std::make_tuple("data/maps/(2)Showdown.scx", 6, 46),
+		MapResourcesTestParameters("data/maps/(2)Showdown.scx", 6, 46),
 
 		// Map without resources
-		std::make_tuple("data/maps/onlydirt.scm", 0, 0),
+		MapResourcesTestParameters("data/maps/onlydirt.scm", 0, 0),
 
 		// Open BW does not support small-sized minerals and gases
 		// See when https://github.com/OpenBW/openbw/issues/19 resolved to unlock
-		// std::make_tuple("data/maps/resources.scm", 3 /* + 9 small gases */, 13 /* + 4 small minerals */),
-		std::make_tuple("data/maps/resources_usual.scm", 3, 13),
+		// MapResourcesTestParameters("data/maps/resources.scm", 3 /* + 9 small gases */, 13 /* + 4 small minerals */),
+		MapResourcesTestParameters("data/maps/resources_usual.scm", 3, 13),
 
 		// In this map all minerals and geysers have just 1% of their total amount 
 		// of 1500 minerals or 5000 gas
-		std::make_tuple("data/maps/resources_smallamount.scm", 3, 13)
+		MapResourcesTestParameters("data/maps/resources_smallamount.scm", 3, 13)
 	})));
 
 
@@ -84,22 +90,22 @@ INSTANTIATE_TEST_CASE_P(
 	SSCAITMaps,
 	MapResourcesInfo,
 	::testing::ValuesIn(getTestData({
-		std::make_tuple("data/maps/sscai/(2)Benzene.scx", 10, 86),
-		std::make_tuple("data/maps/sscai/(2)Destination.scx", 10, 128),
-		std::make_tuple("data/maps/sscai/(2)Heartbreak Ridge.scx", 10, 120),
+		MapResourcesTestParameters("data/maps/sscai/(2)Benzene.scx", 10, 86),
+		MapResourcesTestParameters("data/maps/sscai/(2)Destination.scx", 10, 128),
+		MapResourcesTestParameters("data/maps/sscai/(2)Heartbreak Ridge.scx", 10, 120),
 
-		std::make_tuple("data/maps/sscai/(3)Neo Moon Glaive.scx", 12, 84),
-		std::make_tuple("data/maps/sscai/(3)Tau Cross.scx", 9, 93),
+		MapResourcesTestParameters("data/maps/sscai/(3)Neo Moon Glaive.scx", 12, 84),
+		MapResourcesTestParameters("data/maps/sscai/(3)Tau Cross.scx", 9, 93),
 
-		std::make_tuple("data/maps/sscai/(4)Andromeda.scx", 14, 126),
-		std::make_tuple("data/maps/sscai/(4)Circuit Breaker.scx", 12, 124),
-		std::make_tuple("data/maps/sscai/(4)Electric Circuit.scx", 12, 148),
-		std::make_tuple("data/maps/sscai/(4)Empire of the Sun.scm", 14, 114),
-		std::make_tuple("data/maps/sscai/(4)Fighting Spirit.scx", 14, 108),
-		std::make_tuple("data/maps/sscai/(4)Icarus.scm", 12, 96),
-		std::make_tuple("data/maps/sscai/(4)Jade.scx", 12, 96),
-		std::make_tuple("data/maps/sscai/(4)La Mancha1.1.scx", 12, 96),
-		std::make_tuple("data/maps/sscai/(4)Python.scx", 12, 110),
-		std::make_tuple("data/maps/sscai/(4)Roadrunner.scx", 12, 96)
+		MapResourcesTestParameters("data/maps/sscai/(4)Andromeda.scx", 14, 126),
+		MapResourcesTestParameters("data/maps/sscai/(4)Circuit Breaker.scx", 12, 124),
+		MapResourcesTestParameters("data/maps/sscai/(4)Electric Circuit.scx", 12, 148),
+		MapResourcesTestParameters("data/maps/sscai/(4)Empire of the Sun.scm", 14, 114),
+		MapResourcesTestParameters("data/maps/sscai/(4)Fighting Spirit.scx", 14, 108),
+		MapResourcesTestParameters("data/maps/sscai/(4)Icarus.scm", 12, 96),
+		MapResourcesTestParameters("data/maps/sscai/(4)Jade.scx", 12, 96),
+		MapResourcesTestParameters("data/maps/sscai/(4)La Mancha1.1.scx", 12, 96),
+		MapResourcesTestParameters("data/maps/sscai/(4)Python.scx", 12, 110),
+		MapResourcesTestParameters("data/maps/sscai/(4)Roadrunner.scx", 12, 96)
 	})));
 #endif
