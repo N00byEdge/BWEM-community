6d581fa816366b8e4dfd16b55783fcef48393b78
Christian McCrave <christianmccrave@gmail.com>
2019-08-07 23:37:26 -0400

    Added fully walkable tiles dictating altitude, readded buildable checks

--- BWEM/include/tiles.h
+++ BWEM/include/tiles.h
@@ -7,7 +7,9 @@
 //
 //////////////////////////////////////////////////////////////////////////
 
-#pragma once
+
+#ifndef BWEM_TILES_H
+#define BWEM_TILES_H
 
 #include <BWAPI.h>
 #include "area.h"
@@ -78,7 +80,6 @@ public:
 //	Details: The functions below are used by the BWEM's internals
 
 	void				SetWalkable(bool walkable)	{ m_areaId = (walkable ? -1 : 0); m_altitude = (walkable ? -1 : 1); }
-	void				SetBuildable(bool buildable){ m_areaId = (buildable ? -1 : 0); }
 	bool				SeaOrLake() const			{ return m_altitude == 1; }
 	void				SetSea()					{ bwem_assert(!Walkable() && SeaOrLake()); m_altitude = 0; }
 	void				SetLake()					{ bwem_assert(!Walkable() && Sea()); m_altitude = -1; }
@@ -248,3 +249,7 @@ struct PositionOfTile<MiniTile>
 
 
 } // namespace BWEM
+
+
+#endif
+
